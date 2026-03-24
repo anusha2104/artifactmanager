2597efda57fc2250b56690b63144592cfb32bec6
anusha2104 <anushamittal.ind@gmail.com>
2026-03-24 22:59:59 +0530

    Refactor database connection parameters

--- src/db.py
+++ src/db.py
@@ -1,14 +1,11 @@
 import psycopg2
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 def get_connection():
     return psycopg2.connect(
-        host=os.getenv("DB_HOST"),
-        database=os.getenv("DB_NAME"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD"),
-        port=os.getenv("DB_PORT")
+        host=os.getenv("PGHOST"),
+        database=os.getenv("PGDATABASE"),
+        user=os.getenv("PGUSER"),
+        password=os.getenv("PGPASSWORD"),
+        port=os.getenv("PGPORT")
     )
